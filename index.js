7062f17abdc642a8093c3292bfebc941185415f9
Amos Aderemi <gabrieladeremi.com>
2021-10-20 21:14:43 +0100

    deploying to heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(`listening from port ${PORT}`));
+const server = app.listen(port, () => console.log(`listening from port ${port}`));
 
 module.exports = server;
